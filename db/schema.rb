234ff55e175c81925ab1512bfc609c2f60d6d0c2
Ekow Bentsi-Enchill <ekow.bentsi.enchill@gmail.com>
2021-08-03 09:46:27 +0000

    seeding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_03_085623) do
+ActiveRecord::Schema.define(version: 2021_08_03_093915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_08_03_085623) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "location"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Sport.destroy_all
+College.destroy_all
+Team.destroy_all
+
+# create sports. basketball for now
+sports = [
+    {
+        name: "Basketball",
+        description: "There are three levels, A, B, and C. Players can test out which level is right for them at the beginning of the season, but must have chosen a level by January. Overtime possession determined by a coin flip."
+        location: "Lanman Center"
+    }
+]
+
+Sport.create(sports)
+
+# create colleges
+college_names = ["Benjamin Franklin", "Berkeley", "Branford", "Davenport", "Ezra Stiles", "Grace Hopper", "Jonathan Edwards", "Morse", "Pauli Murray", "Pierson", "Saybrook", "Silliman", "Timothy Dwight", "Trumbull"]
+college_names.each do |name|
+    college = College.new(name: name)
+    college.save
+    count += 1
+end
+
+# create teams
+College.all.each do |c|
+    team = Team.new
+    team.college = c # for now
+    team.name = c.name
+    team.description = Faker::Lorem.paragraph # i will take the descriptions from the intramural websites
+
+    team.save  
+end
