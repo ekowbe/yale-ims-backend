5afe5734df952cc4e64cce1a1780b4b1b54a585a
Ekow Bentsi-Enchill <ekow.bentsi.enchill@gmail.com>
2021-08-12 03:08:49 +0000

    image

--- app/serializers/college_serializer.rb
+++ app/serializers/college_serializer.rb
@@ -1,3 +1,11 @@
 class CollegeSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
+  
   attributes :id, :name, :shield
+
+  def shield
+    rails_blob_path(object.shield, only_path: true) if object.shield.attached?
+  end
+
+  
 end
--- app/serializers/sport_serializer.rb
+++ app/serializers/sport_serializer.rb
@@ -2,4 +2,6 @@ class SportSerializer < ActiveModel::Serializer
   attributes :id, :name, :description
   has_many :teams
   has_many :matches
+
+  
 end
